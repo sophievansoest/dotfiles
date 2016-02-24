eb4ffbfbc6312097075e4e49a0d653903bd95599
Sophie van Soest <projects@k4v.de>
2016-02-24 16:10:53 +0100

    Added i3/i3pystatus_configuration.py

---
+++ i3/i3pystatus_configuration.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+from i3pystatus import Status
+
+status = Status(standalone=True)
+
+status.register("clock",
+        color="#93A8D8",
+        format=" %a %-d %b %H:%M",)
+
+status.register("weather",
+  location_code="GMXX0027",
+  format="{current_temp}",
+  colorize=True,
+  units="metric",
+  on_leftclick= ["chromium http://www.weather.com/de-DE/wetter/heute/l/GMXX0027"]
+)
+
+status.register("mem",
+    color="#E0DA37",
+    format="{used_mem} MiB",)
+
+status.register("cpu_usage",
+    hints = {"markup": "pango"},
+    interval=1,
+    format="<span color=\"#F15E5E\">{usage:02}%</span>",)
+
+status.register("temp",
+    color="#F1AF5F",
+    file="/sys/devices/platform/it87.656/temp1_input",
+    format="{temp:.0f}°C",)
+
+status.register("disk",
+    color="#80D9D8",
+    path="/home",
+    format=" {avail}G",)
+#format="{used}/{total}G [{avail}G]",)
+
+status.register("mpd",
+    host='tank',
+    port=6600,
+    max_field_len=200,
+    max_len=200,
+    format="{artist} - {title} {status}",
+    status={
+        "pause": "▷",
+        "play": "▶",
+        "stop": "◾",
+    },)
+
+status.run()
